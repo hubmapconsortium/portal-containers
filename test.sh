85e2332e32b20a870e15a83aca3b0f9b0b020d49
mccalluc <mccalluc@users.noreply.github.com>
2019-12-12 17:00:54 -0500

    Add a test

---
+++ test.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o pipefail
+
+start() { echo travis_fold':'start:$1; echo $1; }
+end() { set +v; echo travis_fold':'end:$1; echo; echo; }
+die() { set +v; echo "$*" 1>&2 ; exit 1; }
+
+test_docker() {
+  NAME=`basename $PWD`
+  start $NAME
+  docker build --tag $NAME context
+  docker rm -f ${NAME}-container || echo "No container to stop"
+  rm -rf test-output-actual || echo "No directory to delete"
+  mkdir test-output-actual
+  docker run \
+    --name ${NAME}-container \
+    --mount type=bind,source=$PWD/test-input/,target=/input \
+    --mount type=bind,source=$PWD/test-output-actual/,target=/output \
+    $NAME
+    diff -w -r test-output-expected test-output-actual -x .DS_Store \
+      | head -n100 | cut -c 1-100
+  end $NAME
+}
+
+for DIR in *; do
+  if [ -d "$DIR" ]; then
+    pushd $DIR
+      test_docker
+    popd
+  fi
+done
