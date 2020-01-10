5793dac97f74e6b8d9b9d5694b6987420c1d554c
mccalluc <mccalluc@users.noreply.github.com>
2020-01-10 14:26:27 -0500

    Pin dependency: Works with data from Matt

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ green=`tput setaf 2`
 reset=`tput sgr0`
 start() { echo travis_fold':'start:$1; echo "$green$1$reset"; }
 end() { set +v; echo travis_fold':'end:$1; echo; echo; }
-die() { set +v; echo "$*" 1>&2 ; exit 1; }
+die() { set +v; echo "$red$*$reset" 1>&2 ; exit 1; }
 
 test_docker() {
   NAME=`basename $PWD`
