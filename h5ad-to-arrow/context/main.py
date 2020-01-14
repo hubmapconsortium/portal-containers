397e77d4eee6c94b4deef022fb4c028a7afb1b65
Chuck McCallum <mccalluc@users.noreply.github.com>
2020-01-14 11:14:19 -0500

    Mccalluc/csv and json (#9)
    * Remove envvar machinery: Produce csv
    
    * Stub script to create h5ad
    
    * Extract UMAP, correct fixtures
    
    * Ouput JSON in close-to Vitessce format
    
    * conform to schema

--- h5ad-to-arrow/context/main.py
+++ h5ad-to-arrow/context/main.py
@@ -2,27 +2,41 @@ import argparse
 from glob import glob
 from pathlib import Path
 from os import mkdir, environ
+import json
 
 from anndata import read_h5ad
 import pyarrow as pa
+from pandas import DataFrame
 
 
 def h5ad_to_arrow(h5ad_file, arrow_file):
     ann_data = read_h5ad(h5ad_file)
-    dataframe = ann_data.to_df()
-    table = pa.Table.from_pandas(dataframe)
+    umap = ann_data.obsm['X_umap']
+    index = ann_data.obs.index
+
+    df = DataFrame(data=umap, index=index)
+    table = pa.Table.from_pandas(df)
 
     writer = pa.RecordBatchFileWriter(arrow_file, table.schema)
     writer.write(table)
     writer.close()
 
 
-def arrow_to_text(arrow_file, text_file):
+def arrow_to_csv(arrow_file, csv_file):
     df = pa.ipc.open_file(arrow_file).read_pandas()
-    df.to_csv(text_file)
+    df.to_csv(csv_file)
 
 
-envvar_name = 'TEXT_FOR_DIFF'
+def arrow_to_json(arrow_file, json_file):
+    df = pa.ipc.open_file(arrow_file).read_pandas()
+    id_to_pair = {
+        k: { "mappings": {"UMAP": [v[0], v[1]]} }
+        for (k,v) in df.T.to_dict().items()
+    }
+    pretty_json = json.dumps(id_to_pair).replace(']}},', ']}},\n')
+    with open(json_file, 'w') as f:
+        f.write(pretty_json)
+
 
 def main(input_dir, output_dir):
     try:
@@ -31,24 +45,17 @@ def main(input_dir, output_dir):
         pass
     for input in glob(input_dir + '/*.h5ad'):
         input_path = Path(input)
-        output_name = input_path.with_suffix('.arrow').name
-        output_path = Path(output_dir) / output_name
-        h5ad_to_arrow(input_path, output_path)
-
-    if envvar_name in environ:
-        should_be_true = environ[envvar_name]
-        if should_be_true != 'true':
-            raise Exception(f'Only "true" is allowed for ${envvar_name}, not "{should_be_true}".')
-        for input in glob(output_dir + '/*.arrow'):
-            arrow_to_text(input, input + '.txt')
+        arrow_name = input_path.with_suffix('.arrow').name
+        arrow_path = Path(output_dir) / arrow_name
+        h5ad_to_arrow(input_path, arrow_path)
+        arrow_to_csv(arrow_path, arrow_path.with_suffix('.csv'))
+        arrow_to_json(arrow_path, arrow_path.with_suffix('.json'))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description=f'''
-            Transform h5ad into arrow. If the environment variable ${envvar_name}
-            is set to "true", the Arrow will also be translated into a text format
-            for easy diffing.
+            Transform H5AD into Arrow, JSON, and CSV.
         ''')
     parser.add_argument(
         '--input_dir', required=True,
