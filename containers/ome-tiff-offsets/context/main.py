f840672942addb3bf0b776cd24b83a82b3261c77
ilan-gold <ilanbassgold@gmail.com>
2020-04-17 17:32:46 -0400

    Clean up...

--- containers/ome-tiff-offsets/context/main.py
+++ containers/ome-tiff-offsets/context/main.py
@@ -6,33 +6,32 @@ from os import makedirs
 import json
 
 from skimage.external import tifffile
-import xml.dom.minidom
 
 def get_offsets(tiff_filepath):
     with tifffile.TiffFile(tiff_filepath) as tif:
         offsets = [page._offset for page in tif.pages]
     return offsets
 
-
-
 def main(input_dir, output_dir):
     makedirs(output_dir, exist_ok=True)
-    for input_image in glob(input_dir + '/*.ome.tiff') + glob(input_dir + '/*.ome.tif'):
-        offset_values = get_offsets(input_image)
+    for input_path in glob(input_dir + '/*.ome.tiff') + glob(input_dir + '/*.ome.tif'):
+        offset_values = get_offsets(input_path)
         offsets = { "offsetValues": offset_values } 
-        with open(Path(output_dir) / str(Path(input_image).with_suffix('').with_suffix('').name + '.json') , 'w') as f:
+        # Truncate `.ome.tiff`.
+        input_name = Path(input_path).with_suffix('').with_suffix('').name
+        output_path = Path(output_dir) / str(input_name + '.json')
+        with open(output_path, 'w') as f:
             f.write(json.dumps(offsets))
         
 
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-        description='Add offsets to the OMEXML Metadata')
+        description='Create a json file for OME-TIFF offsets')
     parser.add_argument(
         '--input_dir', required=True,
         help='Directory containing ome-tiff files to read')
     parser.add_argument(
         '--output_dir', required=True,
-        help='Directory where ome-tiff files should be written')
+        help='Directory where ome-tiff offsets should be written')
     args = parser.parse_args()
     main(args.input_dir, args.output_dir)
