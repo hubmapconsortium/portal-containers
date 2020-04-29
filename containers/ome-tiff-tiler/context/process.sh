f1f6f49d9e4dd2511b7f11b6bd7bb5b17703ddc7
ilan-gold <ilanbassgold@gmail.com>
2020-04-29 13:47:31 -0400

    Remove --resolutions to allow default handling

--- containers/ome-tiff-tiler/context/process.sh
+++ containers/ome-tiff-tiler/context/process.sh
@@ -10,7 +10,7 @@ do
   BASE_FILE_NAME=$(basename $FILE .ome.tif)
   N5_FILE=/output/$BASE_FILE_NAME.n5/
 
-  SUCCESS=$(/opt/bioformats2raw/bin/bioformats2raw /input/$FILE $N5_FILE  --resolutions 8 --tile_width 512 --tile_height 512)
+  SUCCESS=$(/opt/bioformats2raw/bin/bioformats2raw /input/$FILE $N5_FILE  --tile_width 512 --tile_height 512)
   if [[ $SUCCESS == 1 ]]; then
     die "TIFF-to-n5 failed."
   fi
