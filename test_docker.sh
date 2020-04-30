ff20f4fb596aa5a5a2587819abf51c4a25a569ee
ilan-gold <ilanbassgold@gmail.com>
2020-04-30 10:52:54 -0400

    Remove .bak

--- test_docker.sh
+++ test_docker.sh
@@ -28,7 +28,7 @@ build_test() {
   # hexdump -C test-output-actual/2x2.arrow > test-output-actual/2x2.arrow.hex.txt
   diff -w -r test-output-expected test-output-actual \
       --exclude=.DS_Store --exclude=*.arrow --exclude=*.ome.tif --exclude=*.ome.tiff \
-      --exclude=*ome.xml.bak --exclude=*.ome.xml | head -n100 | cut -c 1-100
+      --exclude=*.ome.xml | head -n100 | cut -c 1-100
 
   # tiff-tiler is a special case: Java rather than Python.
   if [ "$BASENAME" != "ome-tiff-tiler" ]; then
