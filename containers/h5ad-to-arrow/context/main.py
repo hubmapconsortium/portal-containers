3f19e50dfcb6fe162abd4911467fc16b4b97a52b
ilan-gold <43999641+ilan-gold@users.noreply.github.com>
2020-05-15 14:09:57 -0400

    scRNASEQ Update (#31)

--- containers/h5ad-to-arrow/context/main.py
+++ containers/h5ad-to-arrow/context/main.py
@@ -11,10 +11,14 @@ from pandas import DataFrame
 
 def h5ad_to_arrow(h5ad_file, arrow_file):
     ann_data = read_h5ad(h5ad_file)
-    umap = ann_data.obsm['X_umap']
+    umap = ann_data.obsm['X_umap'].transpose()
+    leiden = ann_data.obs['leiden'].to_numpy().astype('uint8')
     index = ann_data.obs.index
 
-    df = DataFrame(data=umap, index=index)
+    df = DataFrame(
+        data={'umap_x': umap[0], 'umap_y': umap[1], 'leiden': leiden},
+        index=index
+    )
     table = pa.Table.from_pandas(df)
 
     writer = pa.RecordBatchFileWriter(arrow_file, table.schema)
@@ -27,15 +31,33 @@ def arrow_to_csv(arrow_file, csv_file):
     df.to_csv(csv_file)
 
 
-def arrow_to_json(arrow_file, json_file):
+def arrow_to_json(arrow_file, **kwargs):
+    umap_json  = kwargs['umap_json']
+    leiden_json  = kwargs['leiden_json']
     df = pa.ipc.open_file(arrow_file).read_pandas()
-    id_to_pair = {
-        k: { "mappings": {"UMAP": [v[0], v[1]]} }
-        for (k,v) in df.T.to_dict().items()
+    df_items = df.T.to_dict().items()
+
+    id_to_umap = {
+        k: {
+            "mappings": {"UMAP": [v['umap_x'], v['umap_y']]},
+            "factors": {"Leiden Cluster": str(int(v['leiden']))}
+        }
+        for (k,v) in df_items
     }
-    pretty_json = json.dumps(id_to_pair).replace(']}},', ']}},\n')
-    with open(json_file, 'w') as f:
-        f.write(pretty_json)
+    pretty_json_umap = json.dumps(id_to_umap).replace('}},', '}},\n')
+    with open(umap_json, 'w') as f:
+        f.write(pretty_json_umap)
+
+    leiden_clusters = sorted(df['leiden'].unique().astype('uint8'))
+    id_to_factors = {
+        'Leiden Cluster': {
+            'map': [str(cluster) for cluster in leiden_clusters],
+            'cells': { k: v['leiden'] for (k,v) in df_items }
+        }
+    }
+    pretty_json_factors = json.dumps(id_to_factors).replace('}},', '}},\n')
+    with open(leiden_json, 'w') as f:
+        f.write(pretty_json_factors)
 
 
 def main(input_dir, output_dir):
@@ -49,7 +71,11 @@ def main(input_dir, output_dir):
         arrow_path = Path(output_dir) / arrow_name
         h5ad_to_arrow(input_path, arrow_path)
         arrow_to_csv(arrow_path, arrow_path.with_suffix('.csv'))
-        arrow_to_json(arrow_path, arrow_path.with_suffix('.json'))
+        arrow_to_json(
+            arrow_file=arrow_path,
+            umap_json=arrow_path.with_suffix('.cells.json'),
+            leiden_json=arrow_path.with_suffix('.factors.json')
+        )
 
 
 if __name__ == '__main__':
