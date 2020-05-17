01bc86991b85a9f1166479c90dcedc4ef398b0fe
ilan-gold <ilanbassgold@gmail.com>
2020-05-17 15:48:04 -0400

    add genes and factors

--- containers/sprm-to-json/context/main.py
+++ containers/sprm-to-json/context/main.py
@@ -60,14 +60,43 @@ def create_cells(tile_str, input_dir, output_dir):
     with open(Path(output_dir) / Path(tile_str).with_suffix(".cells.json"), "w") as f:
         f.write(json.dumps(cells))
 
+def create_factors(tile_str, input_dir, output_dir):
+    factors = {}
+    cluster_input = Path(input_dir) / Path(tile_str + ".ome.tiff-cell_cluster.csv")
+    df = pd.read_csv(cluster_input).set_index("Unnamed: 0")
+    cluster_types = df.columns.values
+    df_items = sprm_to_items(cluster_input)
+    for cluster_type in cluster_types:
+        cluster_names = sorted(df[cluster_type].unique().astype('uint8'))
+        factors[cluster_type] = {
+            'map': [str(cluster) for cluster in cluster_names],
+            'cells': {k: v[cluster_type] for (k,v) in df_items}
+        }
+    with open(Path(output_dir) / Path(tile_str).with_suffix(".factors.json"), "w") as f:
+        f.write(json.dumps(factors))
+    
+def create_genes(tile_str, input_dir, output_dir):
+    genes = {}
+    genes_input = Path(input_dir) / Path(tile_str + ".ome.tiff-cell_channel_total.csv")
+    df = pd.read_csv(genes_input).set_index("Unnamed: 0")
+    gene_types = df.columns.values
+    df_items = sprm_to_items(genes_input)
+    for gene_type in gene_types:
+        genes[gene_type] = {
+            'max': max(df[gene_type]),
+            'cells':  {k: v[gene_type] for (k,v) in df_items}
+        }
+    with open(Path(output_dir) / Path(tile_str).with_suffix(".genes.json"), "w") as f:
+        f.write(json.dumps(genes))
+        
 
 def main(input_dir, output_dir):
     makedirs(output_dir, exist_ok=True)
     for input_file in glob(input_dir + "/*.ome.tiff-cell_polygons_spatial.csv"):
         tile_str = Path(input_file).name.split(".")[0]
         create_cells(tile_str, input_dir, output_dir)
-        # create_factors(tile_str, input_dir, output_dir)
-        # create_genes(tile_str, input_dir, output_dir)
+        create_factors(tile_str, input_dir, output_dir)
+        create_genes(tile_str, input_dir, output_dir)
 
 
 if __name__ == "__main__":
