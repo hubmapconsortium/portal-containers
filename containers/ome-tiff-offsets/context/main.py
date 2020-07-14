76c1d4893dc26a8e97a688e95d779306e8d066ea
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2020-07-14 16:40:53 -0400

    Cleaned up main function so that it can find all OME.TIFFs in path and write them to disk following the same folder structure as the input directory

--- containers/ome-tiff-offsets/context/main.py
+++ containers/ome-tiff-offsets/context/main.py
@@ -2,10 +2,13 @@ import argparse
 from glob import glob
 from pathlib import Path
 from os import makedirs
+from os.path import splitext
 import json
+from os.path import dirname
+from os.path import exists
+from os import makedirs
 
 from skimage.external import tifffile
-
 
 def get_offsets(tiff_filepath):
     with tifffile.TiffFile(tiff_filepath) as tif:
@@ -14,14 +17,20 @@ def get_offsets(tiff_filepath):
 
 def main(input_dir, output_dir):
     makedirs(output_dir, exist_ok=True)
-    for input_path in glob(input_dir + '/*.ome.tiff') + glob(input_dir + '/*.ome.tif'):
+
+    #find all OME.TIFFs in the input directory
+    for input_path in glob(input_dir + '/**/*ome.tif', recursive=True) + glob(input_dir + '/**/*ome.tiff', recursive=True):
         offsets = get_offsets(input_path)
-        # Truncate `.ome.tiff`.
-        input_name = Path(input_path).with_suffix('').with_suffix('').name
-        output_path = Path(output_dir) / str(input_name + '.offsets.json')
+
+        #creates output path for each OME.TIFF
+        output_path = dirname( str(Path(output_dir) / Path( input_path ).relative_to( input_dir )) )
+        if not exists(output_path):
+            makedirs(output_path)
+
+        #sets output filename for JSON file and dumps to disk
+        output_path = str( Path(output_dir) / Path( input_path ).relative_to( input_dir ).with_suffix('').with_suffix('') ) + '.offsets.json'
         with open(output_path, 'w') as f:
             f.write(json.dumps(offsets))
-        
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
