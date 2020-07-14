04fe2238a98aeca754aa404e0889befe494e8f11
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2020-07-14 19:33:19 -0400

    Followed suggestion in code review to reduce casting

--- containers/ome-tiff-offsets/context/main.py
+++ containers/ome-tiff-offsets/context/main.py
@@ -16,15 +16,15 @@ def main(input_dir, output_dir):
     makedirs(output_dir, exist_ok=True)
 
     # Find all OME.TIFFs in the input directory.
-    for input_path in Path(input_dir).glob('**/*.ome.tif'):
+    for input_path in input_dir.glob('**/*.ome.tif'):
         offsets = get_offsets(str(input_path))
 
         # Create output path for each OME.TIFF:
-        new_output_dir = (Path(output_dir) / input_path.relative_to(input_dir)).parent
+        new_output_dir = (output_dir / input_path.relative_to(input_dir)).parent
         new_output_dir.mkdir(exist_ok=True)
 
         # Set output filename for JSON file and dump to disk:
-        output_path = str( Path(output_dir) / Path( input_path ).relative_to( input_dir ).with_suffix('').with_suffix('') ) + '.offsets.json'
+        output_path = str(output_dir / input_path.relative_to(input_dir).with_suffix('').with_suffix(''))+'.offsets.json'
         with open(output_path, 'w') as f:
             f.write(json.dumps(offsets))
 
@@ -32,10 +32,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Create a json file for OME-TIFF offsets')
     parser.add_argument(
-        '--input_dir', required=True,
+        '--input_dir', required=True, type=Path,
         help='Directory containing ome-tiff files to read')
     parser.add_argument(
-        '--output_dir', required=True,
+        '--output_dir', required=True, type=Path,
         help='Directory where ome-tiff offsets should be written')
     args = parser.parse_args()
     main(args.input_dir, args.output_dir)
