5d019de61e7be059445028bde09b9152ac7cd932
ilan-gold <ilanbassgold@gmail.com>
2021-01-28 13:25:11 -0500

    sprm-to-anndata

---
+++ containers/sprm-to-anndata/README.md
@@ -0,0 +1,3 @@
+# sprm-to-anndata
+
+This container takes [SPRM](https://docs.google.com/document/d/1c7UR0Pe1newpVhQY2HEFkfV8O7GAj9Vk4XnuSiSnDeY/edit) output CSVs and converts them into an [AnnData zarr store](https://anndata.readthedocs.io/en/latest/anndata.AnnData.write_zarr.html#anndata.AnnData.write_zarr).
---
+++ containers/sprm-to-anndata/context/main.py
@@ -0,0 +1,86 @@
+import argparse
+from glob import glob
+from pathlib import Path
+from os import makedirs
+import json
+from ast import literal_eval
+
+import anndata
+import zarr
+import pandas as pd
+import numpy as np
+from shapely.geometry import Polygon
+from ast import literal_eval
+
+CELL_X_ANTIGENS_FILE_SUFFIX = ".ome.tiff-cell_channel_mean.csv"
+CLUSTER_FILE_SUFFIX = ".ome.tiff-cell_cluster.csv"
+POLYGON_FILE_SUFFUX = ".ome.tiff-cell_polygons_spatial.csv"
+NUM_VERTICES = 15
+
+
+def downsample_shape(shape):
+    idx = np.round(np.linspace(0, len(shape[0]) - 1, NUM_VERTICES + 1)).astype(int)
+    shape_downsample = np.asarray(shape[0])[idx]
+    poly_downsample = Polygon(shape_downsample).exterior.coords
+    return list(poly_downsample)
+
+
+def get_centroid(shape):
+    poly = Polygon(np.asarray(shape[0]))
+    # poly.centroid.coords is an object that should be converted to a list, but when we get the 0th
+    # coordinate i.e the centroid, it is a tuple which needs to be a list for further processing.
+    return list(list(poly.centroid.coords)[0])
+
+
+def read_sprm_to_pandas(input_file, converters={}):
+    df = pd.read_csv(input_file, converters=converters).set_index("ID")
+    return df
+
+
+def sprm_to_anndata(tile_str, input_dir, output_dir):
+    cell_x_antigen_file = Path(input_dir) / Path(tile_str + CELL_X_ANTIGENS_FILE_SUFFIX)
+    df_cell_x_antigen = read_sprm_to_pandas(cell_x_antigen_file)
+
+    cluster_file = Path(input_dir) / Path(tile_str + CLUSTER_FILE_SUFFIX)
+    # Pandas reads in as int64 by default which can't be interpreted by Javascript.
+    df_cluster = read_sprm_to_pandas(cluster_file).astype("uint8")
+
+    polygon_file = Path(input_dir) / Path(tile_str + POLYGON_FILE_SUFFUX)
+    df_spatial = read_sprm_to_pandas(
+        polygon_file, converters={"Shape": literal_eval}
+    )
+    # Get centroid and downsampled polygon coordinates.
+    df_poly = df_spatial.apply(downsample_shape, axis=1).to_frame(name="Shape")
+    df_xy = df_spatial.apply(get_centroid, axis=1).to_frame(name="Shape")
+
+    adata = anndata.AnnData(
+        X=df_cell_x_antigen.to_numpy(),
+        obsm={
+            "poly": np.array(df_poly.values.tolist()),
+            "xy": np.array(df_xy.values.tolist()),
+        },
+        obs=df_cluster,
+        var=pd.DataFrame(index=df_cell_x_antigen.columns)
+    )
+    adata.write_zarr(str(Path(output_dir) / Path(tile_str + "-anndata.zarr")))
+
+
+def main(input_dir, output_dir):
+    makedirs(output_dir, exist_ok=True)
+    for input_file in glob(input_dir + "/*.ome.tiff-cell_polygons_spatial.csv"):
+        tile_str = Path(input_file).name.split(".")[0]
+        sprm_to_anndata(tile_str, input_dir, output_dir)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description=f'''
+            Transform H5AD into Arrow, JSON, and CSV.
+        ''')
+    parser.add_argument(
+        '--input_dir', required=True,
+        help='directory containing h5ad files to read')
+    parser.add_argument(
+        '--output_dir', required=True,
+        help='directory where arrow files should be written')
+    args = parser.parse_args()
+    main(args.input_dir, args.output_dir)
