31e525c95bd311109a14fbc70ca94387d50aeee1
Ilan <ilanbassgold@gmail.com>
2021-07-27 14:03:58 -0400

    Use TSNE For sprm-to-anndata (#70)
    * Use TSNE For sprm-to-anndata
    
    * CWL Version

--- containers/sprm-to-anndata/context/main.py
+++ containers/sprm-to-anndata/context/main.py
@@ -34,7 +34,6 @@ def read_sprm_to_pandas(input_file, converters={}):
     df = pd.read_csv(input_file, converters=converters).set_index("ID")
     return df
 
-
 def sprm_to_anndata(img_name, input_dir, output_dir):
     cell_x_antigen_file = Path(input_dir) / (img_name + CELL_X_ANTIGENS_FILE_SUFFIX)
     df_cell_x_antigen = read_sprm_to_pandas(cell_x_antigen_file)
@@ -54,11 +53,15 @@ def sprm_to_anndata(img_name, input_dir, output_dir):
     df_poly = df_spatial.apply(downsample_shape, axis=1).to_frame(name="Shape")
     df_xy = df_spatial.apply(get_centroid, axis=1).to_frame(name="Shape")
 
+    tsne_file = Path(input_dir) / (img_name + CLUSTER_FILE_SUFFIX)
+    df_tsne = read_sprm_to_pandas(tsne_file)
+
     adata = anndata.AnnData(
         X=df_cell_x_antigen.to_numpy(),
         obsm={
             "poly": np.array(df_poly.values.tolist()),
             "xy": np.array([x[0] for x in df_xy.values.tolist()]),
+            'tnse': np.array(df_tsne.values.tolist())
         },
         obs=df_cluster,
         var=pd.DataFrame(index=df_cell_x_antigen.columns),
