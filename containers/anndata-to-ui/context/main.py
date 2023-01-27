c4c5498eb827965467e3d0af53b145ef06f4a643
mkeller <7525285+keller-mark@users.noreply.github.com>
2023-01-27 09:08:31 -0500

    Update script

--- containers/anndata-to-ui/context/main.py
+++ containers/anndata-to-ui/context/main.py
@@ -9,7 +9,7 @@ from scipy import sparse
 from anndata import read_h5ad
 
 NUM_MARKER_GENES_TO_VISUALIZE = 5
-VAR_CHUNK_SIZE = 10
+DEFAULT_VAR_CHUNK_SIZE = 10
 SECONDARY_ANALYSIS = "secondary_analysis.h5ad"
 SCVELO_ANNOTATED = "scvelo_annotated.h5ad"
 
@@ -64,7 +64,9 @@ def main(input_dir, output_dir):
         # https://github.com/theislab/anndata/issues/524 
         if isinstance(adata.X, sparse.spmatrix):
             adata.X = adata.X.todense()
-        adata.write_zarr(zarr_path, [adata.shape[0], VAR_CHUNK_SIZE])
+        
+        var_chunk_size = min(adata.shape[1], DEFAULT_VAR_CHUNK_SIZE)
+        adata.write_zarr(zarr_path, chunks=(adata.shape[0], var_chunk_size))
 
 
 if __name__ == "__main__":
